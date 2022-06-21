8c3f05d79975afb75dcefc3be8e50e49c13bd745
Nick Buzzerio <nick.buzzerio@hotmail.com>
2022-06-20 21:49:16 -0400

    added header, footer, and drawer menu template

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        open: 'menuOpen .7s ease-out forwards',
+        close: 'menuClose 1s ease-in-out forwards',
+      },
+      keyframes: {
+        menuOpen: {
+          '0%': {'transform': 'translateX(100%)'},
+          '100%':  {'transform': 'translateX(0)'}
+        },
+        menuClose: {
+          '0%': {'transform': 'translateX(0)'},
+          '100%':  {'transform': 'translateX(100%)'}
+        },
+      },
+    },
   },
   plugins: [],
 }
