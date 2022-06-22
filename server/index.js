b06709ad63e148c8bc5d6618335e8dbaa92d0df7
Nick Buzzerio <nick.buzzerio@hotmail.com>
2022-06-21 20:23:37 -0400

    added express server with next

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const next = require("next")
+const express = require("express")
+
+const dev = process.env.NODE_ENV !== "production"
+const app = next({dev})
+const handle = app.getRequestHandler()
+const { parse } = require("url")
+
+app.prepare().then(() => {
+    const server = express()
+    server.use(express.json())
+
+    server.get('*', (req, res) => {
+        const parsedUrl = parse(req.url, true)
+        handle(req, res, parsedUrl)
+    })
+
+    server.listen(process.env.PORT || 3000, (err) => {
+        if (err) throw err
+        console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`)
+    })
+})
