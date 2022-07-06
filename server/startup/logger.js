2d221b9a873122e22e5671f86166209237549d9d
Nick Buzzerio <nick.buzzerio@hotmail.com>
2022-07-06 09:30:40 -0400

    reformatted console logger

--- server/startup/logger.js
+++ server/startup/logger.js
@@ -5,23 +5,25 @@ const logger = winston.createLogger({
   format: winston.format.combine(
     winston.format.metadata(),
     winston.format.json()
-  ),
-  transports: [
-    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
-    new winston.transports.File({ filename: "logs/combined.log" }),
-    new winston.transports.MongoDB({
-      db: process.env.db,
-      level: "error",
-      options: { useUnifiedTopology: true },
-    }),
-  ],
-});
-if (process.env.NODE_ENV !== "production") {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple()
+    ),
+    transports: [
+      new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+      new winston.transports.File({ filename: "logs/combined.log" }),
+      new winston.transports.MongoDB({
+        db: process.env.db,
+        level: "error",
+        options: { useUnifiedTopology: true },
+      }),
+    ],
+  });
+  if (process.env.NODE_ENV !== "production") {
+    logger.add(
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.timestamp(),
+          winston.format.align(),
+          winston.format.printf(info => `${info.level} -- ${info.timestamp}:${info.message}`)
       ),
     })
   );
